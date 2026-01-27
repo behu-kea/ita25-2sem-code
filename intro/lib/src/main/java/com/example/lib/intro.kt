34fb9622b7bb4a97a634b79f08a1f5094efda877
Benjamin Hughes <behu@kea.dk>
2026-01-27 11:18:01 +0100

    asd

--- intro/lib/src/main/java/com/example/lib/intro.kt
+++ intro/lib/src/main/java/com/example/lib/intro.kt
@@ -1,14 +1,27 @@
 package com.example.lib
 
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.sqrt
+
 fun main() {
-    println("asd")
+    var age: Int
+    //println("asd")
+    val name = "benjamin"
+    val secondCharacterInBenjamin: Char = name.get(0)
+
+
     val numbers: List<Int> = listOf(1, 2, 3, 4, 5);
     val transformedList = getTransformList(numbers)
-    println(transformedList)
+    //println(transformedList)
     for (i in 1..5) {
         if (i == 3) break
-        println(i)
+        //println(i)
     }
+
+    opgave2()
+
+    opgave7()
 }
 
 fun getTransformList(array: List<Int>): MutableList<Int> {
@@ -25,3 +38,31 @@ fun getTransformList(array: List<Int>): MutableList<Int> {
     return transformedArray
 }
 
+
+fun opgave2() {
+    val name: String = "benjamin"
+    val nameUppercase = name.uppercase()
+    println(nameUppercase)
+    val characterOnIndex3:Char = nameUppercase.get(3)
+    println(characterOnIndex3)
+    println("hej " + " hej igen")
+    val string1 = "name"
+    val string2 =  "NaMe"
+
+    println(string1.equals(string2, ignoreCase = true))
+}
+
+
+fun opgave7() {
+    for (i in 1..100) {
+        println(i)
+        val isEven = i % 2 == 0
+        val squareRoot = sqrt(i.toDouble())
+        val isPerfect = ceil(squareRoot/3) == floor(squareRoot/3)
+        if(isEven && isPerfect) {
+            println(i)
+        }
+        println(isPerfect)
+
+    }
+}
